30cdc7570c66f394cb52d4b54631e49046354802
Saleh <saleh-aj@hotmail.com>
2022-12-04 23:00:07 +0200

    Building About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,49 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/Saleh.jpg";
+import { MdWorkOutline } from "react-icons/md";
+import { RiBook2Line } from "react-icons/ri";
+import { BsCodeSlash } from "react-icons/bs";
 
 export default function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="Saleh About Img" />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <MdWorkOutline />
+              <h5>Experience</h5>
+              <small>Junior Developer</small>
+            </article>
+            <article className="about__card">
+              <RiBook2Line />
+              <h5>Education</h5>
+              <small>B.Sc. Degree</small>
+            </article>
+            <article className="about__card">
+              <BsCodeSlash />
+              <h5>Projects</h5>
+              <small>4</small>
+            </article>
+          </div>
+          <p>
+            My name is Muhammad Saleh Ajam, and I have a Bachelor of Science
+            Degree in Informatics Engineering. I have been awarded Al-Bassel
+            Prize for being one of the top students at university. I love coding
+            and pick up colors for my designs. My favotite drink is black
+            coffee, and Elon Musk is my favorite celebrity.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
