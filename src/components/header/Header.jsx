fce24a6c3e58c6ee8e0972421ef8126a38c791c2
Saleh <saleh-aj@hotmail.com>
2022-12-04 08:51:11 +0200

    Building Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/Saleh.jpg";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Muhammad Saleh Ajam</h1>
+        <h5 className="text-light">Informatics Engineer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="Saleh" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
