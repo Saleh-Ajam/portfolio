edc20e921e0e1804abaeebf241e17b9b539c1867
Saleh <saleh-aj@hotmail.com>
2022-12-05 07:35:06 +0200

    Styling Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -10,11 +10,12 @@ import {
   SiRobotframework,
   SiPug,
 } from "react-icons/si";
-import { BsBootstrap } from "react-icons/bs";
-import { IoLogoNodejs, IoLogoAndroid, IoMdImages } from "react-icons/io";
+import { BsBootstrap, BsFillHddNetworkFill } from "react-icons/bs";
+import { IoLogoNodejs, IoMdImages } from "react-icons/io";
 import { TbBrandReactNative, TbMathFunction } from "react-icons/tb";
-import { GiArtificialIntelligence } from "react-icons/gi";
-import { FaPython } from "react-icons/fa";
+import { GiArtificialIntelligence, GiMetalGolemHead } from "react-icons/gi";
+import { FaPython, FaDatabase, FaAndroid } from "react-icons/fa";
+import { TiDeviceTablet } from "react-icons/ti";
 
 export default function Experience() {
   return (
@@ -26,99 +27,162 @@ export default function Experience() {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <ImHtmlFive2 />
-              <h4>HTML</h4>
-              <small className="text-light">Intermediate</small>
+              <ImHtmlFive2 className="experience__details__icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <RiCss3Fill />
-              <h4>CSS</h4>
-              <small className="text-light">Intermediate</small>
+              <RiCss3Fill className="experience__details__icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <SiJavascript />
-              <h4>JavaScript</h4>
-              <small className="text-light">Intermediate</small>
+              <SiJavascript className="experience__details__icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <GrReactjs />
-              <h4>ReactJS</h4>
-              <small className="text-light">Intermediate</small>
+              <GrReactjs className="experience__details__icon" />
+              <div>
+                <h4>ReactJS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsBootstrap />
-              <h4>Bootstrap</h4>
-              <small className="text-light">Biggener</small>
+              <BsBootstrap className="experience__details__icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Biggener</small>
+              </div>
             </article>
             <article className="experience__details">
-              <SiMaterialui />
-              <h4>Material UI</h4>
-              <small className="text-light">Biggener</small>
+              <SiMaterialui className="experience__details__icon" />
+              <div>
+                <h4>Material UI</h4>
+                <small className="text-light">Biggener</small>
+              </div>
             </article>
             <article className="experience__details">
-              <SiPug />
-              <h4>Pug</h4>
-              <small className="text-light">Biggener</small>
-            </article>
-          </div>
-        </div>
-        <div className="experience__backend">
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <IoLogoNodejs />
-              <h4>NodeJS</h4>
-              <small className="text-light">Intermediate</small>
+              <SiPug className="experience__details__icon" />
+              <div>
+                <h4>Pug</h4>
+                <small className="text-light">Biggener</small>
+              </div>
             </article>
             <article className="experience__details">
-              <SiMongodb />
-              <h4>MongoDB</h4>
-              <small className="text-light">Intermediate</small>
-            </article>
-          </div>
-        </div>
-        <div className="experince__mobile__development">
-          <h3>Mobile Development</h3>
-          <div className="experience__conten">
-            <article className="experience__details">
-              <IoLogoAndroid />
-              <h4>Android Development /JAVA</h4>
-              <small className="text-light">Begginer</small>
-            </article>
-            <article className="experience__details">
-              <TbBrandReactNative />
-              <h4>React Native</h4>
-              <small className="text-light">Begginer</small>
+              <TiDeviceTablet className="experience__details__icon" />
+              <div>
+                <h4>UX/UI</h4>
+                <small className="text-light">Biggener</small>
+              </div>
             </article>
           </div>
         </div>
         <div className="experince__computer__science">
           <h3>Computer Science</h3>
-          <div className="experience__conten">
+          <div className="experience__content">
             <article className="experience__details">
-              <GiArtificialIntelligence />
-              <h4>Machine Learning</h4>
-              <small className="text-light">Begginer</small>
+              <GiArtificialIntelligence className="experience__details__icon" />
+              <div>
+                <h4>Machine Learning</h4>
+                <small className="text-light">Begginer</small>
+              </div>
             </article>
             <article className="experience__details">
-              <IoMdImages />
-              <h4>Digital Image Processing</h4>
-              <small className="text-light">Begginer</small>
+              <IoMdImages className="experience__details__icon" />
+              <div>
+                <h4>Digital Image Processing</h4>
+                <small className="text-light">Begginer</small>
+              </div>
             </article>
             <article className="experience__details">
-              <TbMathFunction />
-              <h4>Maths</h4>
-              <small className="text-light">Advanced</small>
+              <TbMathFunction className="experience__details__icon" />
+              <div>
+                <h4>Maths</h4>
+                <small className="text-light">Advanced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <SiRobotframework />
-              <h4>Robotics</h4>
-              <small className="text-light">Begginer</small>
+              <SiRobotframework className="experience__details__icon" />
+              <div>
+                <h4>Robotics</h4>
+                <small className="text-light">Begginer</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FaPython />
-              <h4>Python</h4>
-              <small className="text-light">Intermediate</small>
+              <FaPython className="experience__details__icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <GiMetalGolemHead className="experience__details__icon" />
+              <div>
+                <h4>Algorithms</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillHddNetworkFill className="experience__details__icon" />
+              <div>
+                <h4>Networking</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaDatabase className="experience__details__icon" />
+              <div>
+                <h4>DataBases</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoLogoNodejs className="experience__details__icon" />
+              <div>
+                <h4>NodeJS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiMongodb className="experience__details__icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experince__mobile__development">
+          <h3>Mobile Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaAndroid
+                className="experience__details__icon"
+                id="android-icon"
+              />
+              <div>
+                <h4>Android Development /JAVA</h4>
+                <small className="text-light">Begginer</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <TbBrandReactNative className="experience__details__icon" />
+              <div>
+                <h4>React Native</h4>
+                <small className="text-light">Begginer</small>
+              </div>
             </article>
           </div>
         </div>
