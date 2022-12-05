cc911fa8ebcad9e443445815d2653d5236e54feb
Saleh <saleh-aj@hotmail.com>
2022-12-05 09:07:39 +0200

    Styling Portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,59 +16,65 @@ export default function Portfolio() {
             <img src={IMG1} alt="Fourth year project" />
           </div>
           <h3>Easy Market Android Application</h3>
-          {/* <a href="#" className="btn" target="_blank">
+          <div className="portfolio__item__cta">
+            {/* <a href="#" className="btn" target="_blank">
             Source Code
           </a> */}
-          <a
-            href="https://drive.google.com/drive/folders/1n2WUNHhSPzA46r13EokgV3l-_ffsWfjn?usp=sharing"
-            className="btn"
-            target="_blank"
-          >
-            Demo
-          </a>
+            <a
+              href="https://drive.google.com/drive/folders/1n2WUNHhSPzA46r13EokgV3l-_ffsWfjn?usp=sharing"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item__img">
             <img src={IMG2} alt="Fourth year project" />
           </div>
           <h3>Rubik's Cube Robot</h3>
-          {/* <a href="#" className="btn" target="_blank">
+          <div className="portfolio__item__cta">
+            {/* <a href="#" className="btn" target="_blank">
             Source Code
           </a> */}
-          <a
-            href="https://drive.google.com/drive/folders/1mTlUH05gslJsfIDvTllP6i20zRZuaNEL"
-            className="btn"
-            target="_blank"
-          >
-            Demo
-          </a>
+            <a
+              href="https://drive.google.com/drive/folders/1mTlUH05gslJsfIDvTllP6i20zRZuaNEL"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item__img">
             <img src={IMG3} alt="Fourth year project" />
           </div>
           <h3>Natural Tours</h3>
-          <a
-            href="https://documenter.getpostman.com/view/7259292/U16nKj1C"
-            className="btn"
-            target="_blank"
-          >
-            API Doc.
-          </a>
-          <a
-            href="https://natural-tours.onrender.com/"
-            className="btn"
-            target="_blank"
-          >
-            Demo
-          </a>
-          <a
-            href="https://github.com/Saleh-Ajam/natural-tours"
-            className="btn"
-            target="_blank"
-          >
-            Source Code
-          </a>
+          <div className="portfolio__item__cta">
+            <a
+              href="https://documenter.getpostman.com/view/7259292/U16nKj1C"
+              className="btn"
+              target="_blank"
+            >
+              API Doc.
+            </a>
+            <a
+              href="https://github.com/Saleh-Ajam/natural-tours"
+              className="btn"
+              target="_blank"
+            >
+              Source Code
+            </a>
+            <a
+              href="https://natural-tours.onrender.com/"
+              className="btn-primary btn"
+              target="_blank"
+            >
+              Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
