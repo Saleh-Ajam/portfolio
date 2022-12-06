dd153823beabdfdc9b06cb3aed7db91bcd61c108
Saleh <saleh-aj@hotmail.com>
2022-12-06 04:48:30 +0200

    Building Contact Component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import "./contact.css";
+import { MdAlternateEmail } from "react-icons/md";
+import { IoLogoLinkedin } from "react-icons/io";
+import { BsGithub } from "react-icons/bs";
 
 export default function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdAlternateEmail />
+            <h4>E-mail</h4>
+            <h5>salehajam0@gmail.com</h5>
+            <a href="mailto:salehajam0@gmail.com">Send E-mail</a>
+          </article>
+          <article className="contact__option">
+            <IoLogoLinkedin />
+            <h4>Linked-in</h4>
+            <h5>Muhammad Saleh Ajam</h5>
+            <a href="https://www.linkedin.com/in/muhammad-saleh-ajam-1487b8145/">
+              Send Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsGithub />
+            <h4>Github</h4>
+            <h5>Saleh-Ajam</h5>
+            <a href="https://github.com/Saleh-Ajam">Send E-mail</a>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 }
