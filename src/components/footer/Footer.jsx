7500560dc848fb2237cc3fbf9299db06e669c27b
Saleh <saleh-aj@hotmail.com>
2022-12-06 05:17:40 +0200

    Building Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,55 @@
 import React from "react";
 import "./footer.css";
+import { BsTwitter, BsGithub } from "react-icons/bs";
+import { MdAlternateEmail } from "react-icons/md";
 
+import {} from "react-icons/bs";
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <section id="footer">
+      <footer>
+        <a href="#" className="footer__logo">
+          M. Saleh Ajam
+        </a>
+        <ul className="permalinks">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a href="https://twitter.com/saleh_ref">
+            <BsTwitter />
+          </a>
+          <a href="https://github.com/Saleh-Ajam">
+            <BsGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/muhammad-saleh-ajam-1487b8145/">
+            <MdAlternateEmail />
+          </a>
+        </div>
+        <div className="footer__copyright">
+          <small>&copy; Muhammad Saleh Ajam. All rights reversed.</small>
+        </div>
+      </footer>
+    </section>
+  );
 }
